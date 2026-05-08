13bda2890c9bb2da91f86d1b065b6155142fc8f3
wisteria-tiger <dartingsparrow77@icloud.com>
2026-05-08 15:32:38 -0500

    Added basic movement

--- main.lua
+++ main.lua
@@ -20,10 +20,34 @@ end
 -- Let player restart game from gameover screen 
 
 function love.load()
+    player = {}
+    movementSpeed = 60
+    player.x = 100
+    player.y = 50
 end
 
-function love.update()
+function love.update(dt)
+    movement(dt)
 end
 
 function love.draw()
+    love.graphics.circle("fill", player.x, player.y, 10)
+end
+
+function movement(dt)
+    if love.keyboard.isDown("w") then
+        player.y = player.y - movementSpeed * dt
+    end
+
+    if love.keyboard.isDown("d") then
+        player.x = player.x + movementSpeed * dt
+    end
+
+    if love.keyboard.isDown("s") then
+        player.y = player.y + movementSpeed * dt
+    end
+
+    if love.keyboard.isDown("a") then
+        player.x = player.x - movementSpeed * dt
+    end
 end
